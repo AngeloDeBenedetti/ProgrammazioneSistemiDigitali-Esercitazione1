4edd7afc403ae05d404dbf44307dee2b1b4988ff
Angelo De Benedetti <Debbe05gmail.com>
2026-05-14 12:43:32 +0200

    aggiunti e tracciati .cpp e .h per triangolo isoscele

---
+++ CIsoscelesTriangle.h
@@ -0,0 +1,54 @@
+/*! \file CIsoscelesTriangle.h
+    \brief Declaration of the class IsoscelesTriangle
+    \author Angelo De Benedetti
+*/
+
+#ifndef ISOSCELESTRIANGLE_H
+#define ISOSCELESTRIANGLE_H
+
+#include "CShape.h"
+
+/// @class Isosceles Triangle
+/// @brief to manage an object with the shape of an Isosceles Triangle
+class Triangle : public Shape
+{
+public:
+
+    /// @name CONSTRUCTORS / DESTRUCTOR
+    /// @{
+    IsoscelesTriangle();
+    IsoscelesTriangle(float px, float py, float w, float h);
+    IsoscelesTriangle(const IsoscelesTriangle &r);
+
+    ~IsoscelesTriangle();
+    /// @}
+
+    /// @name OPERATORS
+    /// @{
+    IsoscelesTriangle& operator=(const IsoscelesTriangle &r);
+    bool operator==(const IsoscelesTriangle &r);
+    /// @}
+
+    /// @name BASIC HANDLING
+    /// @{
+    void Init();
+    void Init(const IsoscelesTriangle &r);
+    void Reset();
+    /// @}
+
+    /// @name GETTERS
+    /// @{
+    float GetArea();
+    float GetPerimeter();
+    float GetHDiagonal();
+    float GetVDiagonal();
+	float GetSide();
+    /// @}
+
+    /// @name DEBUG and SERIALIZATION
+    /// @{
+    void Dump(); 
+    /// @}
+};
+
+#endif
