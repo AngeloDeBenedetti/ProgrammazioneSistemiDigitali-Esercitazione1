03f52b9226f9838a671075c5461981cd62a8da6f
Angelo De Benedetti <Debbe05gmail.com>
2026-05-19 15:34:19 +0200

    finito main_grid.cpp, ancora da fare prova compilatore e molto probabilmente correzioni

--- main_grid.cpp
+++ main_grid.cpp
@@ -17,7 +17,9 @@ using namespace std;
 void ShowAllPolygons(Shape* shapes[]);
 void ModifyPolygon(Shape* shapes[]);
 void MovePolygon(Shapes* shapes[]);
-
+void DeletePolygon(Shape* shapes[]);
+void CreatePolygon (Shape* shapes[]);
+void DeleteAllPoly (Shape* shapes[]);
 
 int main()
 {
@@ -42,29 +44,28 @@ int main()
             return;
         }
 
-        switch(opt)
+        switch(opt){
             case 1:
             ShowAllPolygons(shapes);
             break;
             case 2:
-            ModifyPolygon(shapes)
+            ModifyPolygon(shapes);
             break;
             case 3:
-            MovePolygon(shapes)
+            MovePolygon(shapes);
             break;
             case 4:
-
+            CreatePolygon(shapes);
             break;
             case 5:
-
+            DeletePolygon(shapes);
             break;
             case 6:
-
+            DeleteAllPoly (shapes);
             break;
-            
             default:
             cout <<"The selection isn't valid. Please enter a valid selection" << endl;
-            break;
+            }
         
         opt = 0;
     }
@@ -155,32 +156,92 @@ while(true)
         }
         else
         {
+            if(shapes[sel] == NULL)
+            {
+                cout <<"the polygon does not exist"<< endl
+                continue;
+            }
+            
             cout << "Selected object:" << endl;
             shapes[sel]->info();
             cout <<"Enter 1 to confirm or -1 to cancel"
             cin >> ok;
-            switch(ok)
+            switch(ok){
             case 1;
             shapes[sel]->~Shape();
             break;
             case -1:
-            return;
+            continue;
             break
             default:
             ok = 0;
-            break;
+            }
         }
     }
 }
 
-    void CreatePolygon (Shape* shapes[])
+void CreatePolygon (Shape* shapes[])
     {
-        for(int i=0; i++; i < MAX_SHAPES)
+        float x,y,w,h = 0; int sel = 0;
+        for(int i=0; i < MAX_SHAPES; i++)
         {
             if(shapes[i] == NULL)
             {
+                cout <<"Please select the type of object to be created:"<< endl <<"1- Rectangle" << endl << "2- Rhombus"<< endl << "3- Isosceles Triangle"<< endl;
+                cin >> sel >> endl;
+                if(sel =! 1 || sel=! 2 || sel=! 3)
+                {
+                    cout <<"invalid selection."<< endl;
+                    return;
+                }
+                cout <<"please enter new object's x and y position" << endl;
+                cin >> x >> y >> endl;
+                cout << "please enter new object's width and height" << endl;
+                cin >> w >> h >> endl; 
+                switch (sel){
+                    case 1:
+                    shapes[i]= new Rectangle(x, y, w, h);
+                    break;
+                    case 2:
+                    shapes[i]= new Rhombus(x, y, w, h);
+                    break;
+                    case 3:
+                    shapes[i]= new IsoscelesTriangle(x, y, w, h);
+                    break;
+                    default:
+                    sel=0;
+                }
                 
+                if(shapes[i]== NULL)
+                {
+                    cout << "Error. The polygon was not created" << endl;
+                }
+                return;
             }
-
         }
     }
+
+void DeleteAllPoly (Shape* shapes[])
+{
+    int ok = 0;
+    cout <<"Are you sure you want to delete ALL polygons?" << endl << "1 -YES" << "-1 - NO" << endl;
+    cin >> ok >> endl;
+    if(ok =! 1 || ok =! -1)
+    {
+        cout << "invalid input."<< endl;
+    }
+    else if(ok == 1)
+    {
+        for (int i = 0; i < nShapes; i++) 
+        {
+        
+            if(shapes[i] =! NULL)
+                {cout << endl << "Figura [" << i << "]" << endl;
+                shapes[i]->~Shape();}
+        }
+    }
+    else if( ok == -1)
+    {
+        cout << "operation canceled" << endl;
+    }
+}
